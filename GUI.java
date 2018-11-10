426f46befffb79a1df81f5db3f6b0c120de05c50
alexdo21 <alex.do21@yahoo.com>
2018-11-10 12:44:03 -0600

    GUIV2.java

--- GUI.java
+++ GUI.java
@@ -1,12 +1,11 @@
+package application;
+
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class GUI {
-	
-	private static final int VBOX_WIDTH = 150;
-	private static final int HBOX_HEIGHT = 100;
 
 	public static BorderPane setupGUI(BorderPane root) {
 		root.setLeft(setupLeft());
@@ -14,35 +13,41 @@ public class GUI {
 		root.setBottom(setupBottom());
 		root.setTop(setupTop());
 		root.setCenter(setupCenter());
+
 		return root;
 	}
 
 	private static HBox setupBottom() {
 		HBox bottom = new HBox();
+		bottom.setId("hbox-bottom");
 		bottom.getChildren().add(new Label("Bottom"));
 		return bottom;
 	}
 
 	private static HBox setupTop() {
 		HBox top = new HBox();
+		top.setId("hbox-top");
 		top.getChildren().add(new Label("Top"));
 		return top;
 	}
 
 	private static HBox setupCenter() {
 		HBox center = new HBox();
+		center.setId("hbox-center");
 		center.getChildren().add(new Label("Center"));
 		return center;
 	}
 
 	private static VBox setupLeft() {
 		VBox left = new VBox();
+		left.setId("vbox-left");
 		left.getChildren().add(new Label("Left"));
 		return left;
 	}
 
 	private static VBox setupRight() {
 		VBox right = new VBox();
+		right.setId("vbox-right");
 		right.getChildren().add(new Label("Right"));
 		return right;
 	}
